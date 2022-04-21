38279b43531de64c89a1dabcf31d6ad054e1ebd1
rberecka <rachael.berecka@hashicorp.com>
2022-04-21 15:52:21 -0700

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,3 +1,3 @@
 output "p" {
-  value = "${random_pet.pet.id} && ${random_string.random_word.id}"
+  value = "${random_pet.pet.id} && ${random_string.random_word.id} && ${random_string.random_word_2.id}"
 }
