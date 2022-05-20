bc289a1996268307f3bace35623b048d66889392
rberecka <rachael.berecka@hashicorp.com>
2022-05-19 17:34:08 -0700

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,9 +1,11 @@
 variable "word-count" {
   type = number
+  default = 4
   description = "number of words"
 }
 
 variable "word-length" {
   type = number
+  default = 8
   description = "length of random string which i called a word"
 }
